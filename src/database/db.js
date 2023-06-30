9a0be82ea393763a2f145c060521515be699a5d5
Wilson Souza <31577477+WilsonCRVG@users.noreply.github.com>
2023-06-30 03:06:04 -0300

    Update db.js

--- src/database/db.js
+++ src/database/db.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize')
 
 
     
-    const sequelize = new Sequelize('crud_sequelize','root','1346798520',{
+    const sequelize = new Sequelize('crud_sequelize','root','',{
 
     dialect:'mysql',
     host:'localhost'
